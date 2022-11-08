e2b9ae6b1aaeaeccc5d1362cd7221cb6dd4269df
mahbubh0ssain <mahbubju48@gmail.com>
2022-11-08 11:54:12 +0600

    get one service by id added

--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../components/Home/Home";
+import ServiceandRivew from "../components/ServiceandRivew/ServiceandRivew";
 import Services from "../components/Services/Services";
 import Main from "../layout/Main";
 
@@ -15,6 +16,12 @@ export const router = createBrowserRouter([
       {
         path: "/services",
         element: <Services></Services>,
+      },
+      {
+        path: "/services/:id",
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/all-service/${params.id}`),
+        element: <ServiceandRivew></ServiceandRivew>,
       },
     ],
   },
---
+++ src/components/ServiceandRivew/ServiceandRivew.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ServiceandRivew = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default ServiceandRivew;
