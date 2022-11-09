4361186e79fe2505afe47d1754abc45de96588e2
mahbubh0ssain <mahbubju48@gmail.com>
2022-11-09 15:15:18 +0600

    review delete complete

--- src/components/MyReviews/MyReviews.js
+++ src/components/MyReviews/MyReviews.js
@@ -6,37 +6,56 @@ import ReviewTable from "./ReviewTable";
 const MyReviews = () => {
   const { user } = useContext(AuthContext);
   const [reviews, setReviews] = useState([]);
-  console.log(reviews);
+  const [refresh, setRefresh] = useState(true);
   useEffect(() => {
     fetch(`http://localhost:5000/my-reviews?email=${user?.email}`)
       .then((res) => res.json())
       .then((data) => {
         if (data.success) {
           setReviews(data.data);
+          setRefresh(!refresh);
         } else {
           console.log("Reviews not found");
         }
       });
-  }, [user?.email]);
+  }, [user?.email, refresh]);
 
   return (
-    <div className="container my-5">
-      <h4 className="text-center mb-3">My Reviews</h4>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Service name</th>
-            <th>Review</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {reviews.map((review) => (
-            <ReviewTable key={review._id} review={review}></ReviewTable>
-          ))}
-        </tbody>
-      </Table>
+    <div
+      style={{ "min-height": "26vh" }}
+      className="container my-5 position-relative"
+    >
+      {reviews.length > 0 ? (
+        <>
+          <h4 className="text-center mb-3">My Reviews</h4>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Service name</th>
+                <th>Review</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {reviews.map((review) => (
+                <ReviewTable key={review._id} review={review}></ReviewTable>
+              ))}
+            </tbody>
+          </Table>
+        </>
+      ) : (
+        <h3
+          style={{
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50% -50%)",
+          }}
+          className="text-center position-absolute"
+        >
+          No reviews were added yet.
+        </h3>
+      )}
     </div>
   );
 };
--- src/components/MyReviews/ReviewTable.js
+++ src/components/MyReviews/ReviewTable.js
@@ -1,17 +1,46 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
-
+import Swal from "sweetalert2";
 
 const ReviewTable = ({ review }) => {
   const { serviceName, reviewTxt, _id } = review;
 
   const handleDelete = (_id) => {
-    fetch(`http://localhost:5000/delete/${_id}`, {
-      method: "DELETE",
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success",
+        cancelButton: "btn btn-danger",
+      },
+      buttonsStyling: false,
+    });
+    swalWithBootstrapButtons
+      .fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Yes, delete!",
+        cancelButtonText: "No, cancel!",
+        reverseButtons: true,
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          fetch(`http://localhost:5000/delete/${_id}`, {
+            method: "DELETE",
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              if (data.data.acknowledged) {
+                swalWithBootstrapButtons.fire(
+                  "Deleted!",
+                  "Review has been deleted.",
+                  "success"
+                );
+              } else {
+                console.log(data);
+              }
+            });
+        }
       });
   };
   return (
@@ -25,7 +54,7 @@ const ReviewTable = ({ review }) => {
         <Button className="me-2 " variant="primary">
           Edit
         </Button>
-        <Button onClick={handleDelete} variant="danger">
+        <Button onClick={() => handleDelete(_id)} variant="danger">
           Delete
         </Button>
       </td>
