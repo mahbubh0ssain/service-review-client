2a2e14ef4cb6c0e988f654173e35cbbc07f974bb
Md. Mahbub Hossain <mahbubh0ssain.dev@gmail.com>
2022-12-22 11:47:49 +0600

    complete

--- src/components/LatestProjects/LatestProjects.js
+++ src/components/LatestProjects/LatestProjects.js
@@ -4,11 +4,10 @@ const LatestProjects = () => {
   return (
     <div className="shadow rounded-3 p-4 my-4">
       <div className="text-center text-primary">
-        <h3>Latest Projects</h3>
-        <h6>Believe in quality not quantity.</h6>
+        <h3 className="mb-3">Latest Projects</h3>
       </div>
       <div className="row row-cols-1 row-cols-md-1 row-cols-lg-3">
-        <div className="mb-3 bg-image hover-zoom">
+        <div className="mb-3">
           <img
             className="img-fluid rounded-3 w-100 "
             src="https://833nycplumber.com/wp-content/uploads/2018/08/Depositphotos_25630745_xl-2015.jpg"
