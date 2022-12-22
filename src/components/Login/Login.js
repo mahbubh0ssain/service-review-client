94b85f98a3621845f6d326854532ee7e3d3276c0
Md. Mahbub Hossain <mahbubh0ssain.dev@gmail.com>
2022-12-22 10:31:56 +0600

    login section redesigned

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { Link, useNavigate, useLocation } from "react-router-dom";
@@ -8,7 +8,7 @@ import { useTitle } from "../../Hooks/UseTitle/UseTitle";
 
 const Login = () => {
   useTitle("Login");
-  const { loginUser, googleLogIn, user, githubLogin } = useContext(AuthContext);
+  const { loginUser, googleLogIn, githubLogin } = useContext(AuthContext);
   const navigate = useNavigate();
   const location = useLocation();
   const from = location.state?.from?.pathname || "/";
@@ -112,69 +112,71 @@ const Login = () => {
   };
 
   return (
-    <div
-      className="container p-5 shadow my-5 rounded-4"
-      style={{ "max-width": "560px" }}
-    >
-      <h4 className="text-center">Log in now</h4>
-      <Form onSubmit={handleSubmit} className="container ">
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control name="email" type="email" placeholder="Enter email" />
-        </Form.Group>
+    <div style={{ minHeight: "77vh" }}>
+      <div
+        className="container p-5 shadow my-5 rounded-4"
+        style={{ "max-width": "560px" }}
+      >
+        <h4 className="text-center">Log in now</h4>
+        <Form onSubmit={handleSubmit} className="container ">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control name="email" type="email" placeholder="Enter email" />
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            name="password"
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button className="w-100" variant="primary" type="submit">
-          Login
-        </Button>
-      </Form>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              name="password"
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button className="w-100" variant="primary" type="submit">
+            Login
+          </Button>
+        </Form>
 
-      <p className="text-center my-2">
-        New to <span className="fw-bold"> Mr. Plumber?</span>
-        <Link to="/signup" className=" fw-bold ms-2">
-          Signup
-        </Link>
-      </p>
-      <div className="mt-3 row row-cols-1 row-cols-md-1 row-cols-lg-2 mx-auto ">
-        <div className=" mb-3">
-          <Button
-            onClick={loginGoogle}
-            className="w-100"
-            variant="outline-primary"
-          >
-            <img
-              className="img-fluid me-2 "
-              style={{ height: "30px", width: "30px" }}
-              src="https://i.ibb.co/nCkjPRN/google.png"
-              alt="..."
-            />
-            Sign in with Goggle
-          </Button>
-        </div>
-        <div className=" mb-3">
-          <Button
-            onClick={loginGithub}
-            className="w-100"
-            variant="outline-secondary"
-          >
-            <img
-              className="img-fluid me-2"
-              style={{ height: "30px", width: "30px" }}
-              src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
-              alt="..."
-            />
-            Sign in with Github
-          </Button>
+        <p className="text-center my-2">
+          New to <span className="fw-bold"> Mr. Plumber?</span>
+          <Link to="/signup" className=" fw-bold ms-2">
+            Signup
+          </Link>
+        </p>
+        <div className="mt-3 row row-cols-1 row-cols-md-1 row-cols-lg-2 mx-auto ">
+          <div className=" mb-3">
+            <Button
+              onClick={loginGoogle}
+              className="w-100"
+              variant="outline-primary"
+            >
+              <img
+                className="img-fluid me-2 "
+                style={{ height: "30px", width: "30px" }}
+                src="https://i.ibb.co/nCkjPRN/google.png"
+                alt="..."
+              />
+              Sign in with Goggle
+            </Button>
+          </div>
+          <div className=" mb-3">
+            <Button
+              onClick={loginGithub}
+              className="w-100"
+              variant="outline-secondary"
+            >
+              <img
+                className="img-fluid me-2"
+                style={{ height: "30px", width: "30px" }}
+                src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+                alt="..."
+              />
+              Sign in with Github
+            </Button>
+          </div>
         </div>
       </div>
     </div>
